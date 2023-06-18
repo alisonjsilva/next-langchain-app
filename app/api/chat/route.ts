a17797cf7d8f687c2157b5563ca6376c4b213c44
Alison Silva <alison.silva@wundermanthompson.com>
2023-06-18 20:24:34 +0100

    Update to use Pinecone Vectors

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,28 +2,66 @@ import { StreamingTextResponse, LangChainStream, Message } from 'ai'
 import { CallbackManager } from 'langchain/callbacks'
 import { ChatOpenAI } from 'langchain/chat_models/openai'
 import { AIChatMessage, HumanChatMessage } from 'langchain/schema'
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { HNSWLib } from 'langchain/vectorstores';
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { PineconeClient, ScoredVector } from "@pinecone-database/pinecone";
+import { VectorDBQAChain } from "langchain/chains";
 
 export const runtime = 'edge'
 
+
+
 export async function POST(req: Request) {
   const { messages } = await req.json()
+  // const vectorStore = await HNSWLib.load("Awards_Ratings__TO_IMPORT.index", new OpenAIEmbeddings());
+  const client = new PineconeClient();
+  await client.init({
+    environment: process.env.PINECONE_ENVIRONMENT!,
+    apiKey: process.env.PINECONE_API_KEY!,
+  });
 
+  const pineconeIndex = client.Index(process.env.PINECONE_INDEX as string);
+
+  const vectorStore = await PineconeStore.fromExistingIndex(
+    new OpenAIEmbeddings(),
+    { pineconeIndex }
+  );
+
+  // const results = await vectorStore.similaritySearch(
+  //   "How many Awards?",
+  // );
+  // console.log(results);
+  let streamedResponse = "";
   const { stream, handlers } = LangChainStream()
 
-  const llm = new ChatOpenAI({
+  const llmStreamModel = new ChatOpenAI({
     streaming: true,
-    callbackManager: CallbackManager.fromHandlers(handlers)
+    callbackManager: CallbackManager.fromHandlers(handlers),
   })
 
-  llm
-    .call(
-      (messages as Message[]).map(m =>
-        m.role == 'user'
-          ? new HumanChatMessage(m.content)
-          : new AIChatMessage(m.content)
-      )
-    )
-    .catch(console.error)
+
+  const query = (messages as Message[]).map(m =>
+    m.role == 'user'
+      ? m.content
+      : ""
+  )?.at(-1);
+  console.log(query);
+
+  const nonStreamingModel = new ChatOpenAI({});
+  const chain = VectorDBQAChain.fromLLM(llmStreamModel, vectorStore, nonStreamingModel);
+  const chainResult = await chain.call({ query: query });
+
+
+  // llmStreamModel
+  //   .call(
+  //     (messages as Message[]).map(m =>
+  //       m.role == 'user'
+  //         ? new HumanChatMessage(m.content)
+  //         : new AIChatMessage(m.content)
+  //     )
+  //   )
+  //   .catch(console.error)
 
   return new StreamingTextResponse(stream)
 }
